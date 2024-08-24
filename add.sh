7e1e2f77f250d4c01c90bc52dce404772c03c814
Ramki2343 <139844652+Ramki2343@users.noreply.github.com>
2024-08-24 11:14:28 +0530

    Update add.sh

--- add.sh
+++ add.sh
@@ -8,8 +8,8 @@ echo "Enter the first number:100 "
 echo "Enter the second number: 50"
 
 
-# Multiply numbers
-result=$((100 * 50))
+# Divide numbers
+result=$((100 / 50))
 
 # Output the result
-echo "The result of $1100 * $550 is: $result"
+echo "The result of $1100 / $550 is: $result"
